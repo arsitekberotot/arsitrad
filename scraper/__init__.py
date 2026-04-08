8df0b8a9bb2fff61a35a0708bb276e78b527f41a
Hanif Riza <88432964+arsitekberotot@users.noreply.github.com>
2026-04-08 13:28:38 +0700

    Delete scraper directory

--- scraper/__init__.py
+++
@@ -1,4 +0,0 @@
-# Scraper module for Arsitrad
-from .pasal_scraper import RegulationScraper, DOMAINS, RegulationMeta, ParsedRegulation
-
-__all__ = ["RegulationScraper", "DOMAINS", "RegulationMeta", "ParsedRegulation"]
